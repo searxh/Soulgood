175106bf5ea2f2cdcb688050f81e94eb486c38cf
search <search35453@gmail.com>
2022-12-19 14:40:53 +0700

    background changes

--- src/lib/Background.tsx
+++ src/lib/Background.tsx
@@ -1,21 +1,17 @@
 import React from "react";
 
-export function Hills({ name }: { name: string }) {
+export function Bg({ name }: { name: string }) {
     const [image, setImage] = React.useState<string>("");
     React.useLayoutEffect(() => {
         if (name !== undefined) {
-            setImage("/assets/bg_" + name + ".jpg");
+            setImage("/assets/bg_" + name);
         }
     }, [name]);
-    return <img src={image} className="flex-1 object-cover" alt="" />;
-}
-
-export function Forest({ name }: { name: string }) {
-    const [image, setImage] = React.useState<string>("");
-    React.useLayoutEffect(() => {
-        if (name !== undefined) {
-            setImage("/assets/bg_" + name + ".jpg");
-        }
-    }, [name]);
-    return <img src={image} className="flex-1 object-cover" alt="" />;
+    return (
+        <img
+            src={image}
+            className="flex-1 object-cover bg-yellow-100 opacity-50"
+            alt=""
+        />
+    );
 }
