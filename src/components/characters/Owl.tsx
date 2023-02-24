2532e35f2ce7d5b116ff80ef56dd6d37d3d45a74
search <search35453@gmail.com>
2023-02-25 00:53:32 +0700

    feat: Processed owl images + size change

--- src/components/characters/Owl.tsx
+++ src/components/characters/Owl.tsx
@@ -10,14 +10,14 @@ export default function Owl({
     const [image, setImage] = React.useState<string>("");
     React.useLayoutEffect(() => {
         if (state !== undefined) {
-            setImage("/assets/Character/Owl/Owl_" + state + ".PNG");
+            setImage("/assets/Character/Owl/Owl_" + state + ".png");
         }
     }, [state, className]);
     return (
         <img
             src={image}
             className={`absolute drop-shadow-md m-auto
-            animate-bob z-10 w-[600px] max-w-[80%] transition duration-300`}
+            animate-bob z-10 w-full max-w-[400px] transition duration-300`}
             alt=""
             style={className}
         />
