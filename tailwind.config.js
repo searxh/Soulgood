8c9a69a24a761cdadce2c66aff9479301279bce9
search <search35453@gmail.com>
2023-02-28 21:52:44 +0700

    feat: added new audio track

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    fontSize: {
-      '2xs': '.625rem',
-      'xs': '.75rem',
-      'sm': '.875rem',
-      'base': '1rem',
-      'lg': '1.125rem',
-      'xl': '1.25rem',
-      '2xl': '1.5rem',
-      '3xl': '1.875rem',
-      '4xl': '2.25rem',
-      '5xl': '3rem',
-      '6xl': '4rem',
-      '7xl': '5rem',
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        fontSize: {
+            "2xs": ".625rem",
+            xs: ".75rem",
+            sm: ".875rem",
+            base: "1rem",
+            lg: "1.125rem",
+            xl: "1.25rem",
+            "2xl": "1.5rem",
+            "3xl": "1.875rem",
+            "4xl": "2.25rem",
+            "5xl": "3rem",
+            "6xl": "4rem",
+            "7xl": "5rem",
+        },
+        extend: {
+            fontFamily: {
+                mitr: "Mitr",
+            },
+            scale: {
+                "-1": "-1",
+            },
+            keyframes: {
+                bob: {
+                    "0%, 100%": {
+                        transform: ["rotate(-2deg)", "translateY(-2.5%)"],
+                    },
+                    "50%": { transform: ["rotate(2deg)", "translateY(2.5%)]"] },
+                },
+            },
+            animation: {
+                bob: "bob 5s ease-in-out infinite",
+            },
+        },
     },
-    extend: {
-      fontFamily:{
-        "mitr":"Mitr"
-      },
-      scale:{
-        "-1":"-1"
-      },
-      keyframes:{
-        bob:{
-          '0%, 100%': { transform: ['rotate(-2deg)','translateY(-2.5%)'] },
-          '50%': { transform: ['rotate(2deg)','translateY(2.5%)]'] },
-        }
-      },
-      animation: {
-        bob: 'bob 5s ease-in-out infinite'
-      }
-    },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
