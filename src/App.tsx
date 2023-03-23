3eb40f403f48fc136238a6a91186e33b7b369989
search <search35453@gmail.com>
2023-03-23 21:52:01 +0700

    feat: Added firebase instance + GA + uid cookie

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,15 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Story from "./pages/Story";
 import Result from "./pages/Result";
+import generateUID from "./lib/utilities/GenerateUID";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize(process.env.REACT_APP_FIREBASE_MEASUREMENT_ID as string);
 
 function App() {
+    React.useEffect(() => {
+        generateUID();
+    }, []);
     return (
         <Routes>
             <Route path="/" element={<Home />} />
